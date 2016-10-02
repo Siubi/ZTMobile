12dfa5b703e4bb093fe6208d2a52a437ac1f483b
Bianca Golik <bianka.golik@gmail.com>
2016-10-02 21:52:10 +0200

    pierwszy komicik

--- ZTMobile/ZTMobile/MainActivity.cs
+++ ZTMobile/ZTMobile/MainActivity.cs
@@ -24,7 +24,7 @@ namespace ZTMobile
             // and attach an event to it
             Button button = FindViewById<Button>(Resource.Id.MyButton);
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            button.Click += delegate { button.Text = string.Format("{0} clicks!\n no i co z tego", count++); };
         }
     }
 }
